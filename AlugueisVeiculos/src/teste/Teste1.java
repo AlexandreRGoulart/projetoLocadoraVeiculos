e048a4b8c6ffa127d2d8860c595ebd24970bd0d3
Alexandre <goulart726@gmail.com>
2019-09-26 16:38:28 -0300

    implementacao da classe generica,4.4 kk

--- AlugueisVeiculos/src/teste/Teste1.java
+++ AlugueisVeiculos/src/teste/Teste1.java
@@ -12,6 +12,7 @@ import model.Relatorio;
 import model.Veiculo;
 
 public class Teste1 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 // Carro(nome, precoPorAluguel, cor, chassi, placa, modelo, ano, motor, lugares, portas)
 // Moto(nome, precoPorAluguel, cor, chassi, placa, modelo, ano, motor, cilindradas)
@@ -19,9 +20,11 @@ public class Teste1 {
 		
 		System.out.println("Testando...");
 		Cliente c = new Cliente("Fulano", "111.222.333-44", "987654321", "Rua1");
+		Carro ca1 = new Carro("carro", 1000, "branco", "2145896", "POO-2019", "CARRO" , 2019, 1, 5, 4);
 		
-		/*
-		c.addCarro("carro", 1000, "branco", "2145896", "POO-2019", "CARRO" , 2019, 1, 5, 4);	
+		/*c.addCarro("carro", 1000, "branco", "2145896", "POO-2019", "CARRO" , 2019, 1, 5, 4);*/
+		
+		/*	
 		c.addMoto("moto", 100, "azul", "458965", "PPP-2019", "MOTO" , 2019, 1, 600);
 		c.addBicicleta("bicicleta", 10, "vermelha", 4);
 		c.addManutencao("12/02/2000", "13:00", 200);
@@ -31,8 +34,8 @@ public class Teste1 {
 		System.out.println(c.toString());
 		System.out.println("_____________________________________\n");
 		/* esse .get() vem da onde?*/
-		Generico g = new Generico();
-		c.setProdutos((List<? extends Veiculo>) g.getElemento());
+		//Generico g = new Generico();
+		c.setProdutos((List<? extends Veiculo>) ca1);
 		Produto p2 = c.getProdutos().get(1);
 		Produto p3 = c.getProdutos().get(2);
 		Produto p4 = c.getProdutos().get(3);
@@ -44,7 +47,7 @@ public class Teste1 {
 		System.out.println("Gerando relatorio...");
 
 		Relatorio r = new Relatorio();
-		System.out.println(r.tipoProduto(p1) + ": " + r.calcularGastos(p1));
+		//System.out.println(r.tipoProduto(p1) + ": " + r.calcularGastos(p1));
 		System.out.println(r.tipoProduto(p2) + ": " + r.calcularGastos(p2));
 		System.out.println(r.tipoProduto(p3) + ": " + r.calcularGastos(p3));
 		System.out.println(r.tipoProduto(p4) + ": " + r.calcularGastos(p4));
