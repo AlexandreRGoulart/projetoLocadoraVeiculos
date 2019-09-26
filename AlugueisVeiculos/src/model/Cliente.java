d7dbaa188b30ab3201a938e0e032c385f19fa573
Alexandre <goulart726@gmail.com>
2019-09-26 13:52:47 -0300

    commit atualizado

--- AlugueisVeiculos/src/model/Cliente.java
+++ AlugueisVeiculos/src/model/Cliente.java
@@ -8,19 +8,22 @@ public class Cliente {
 	private String cpf;
 	private String telefone;
 	private String endereco;
-<<<<<<< HEAD
+/*<<<<<<< HEAD
 	private List<Produto> produtos;
+	
 =======
-	private List<IProduto> produtos;
-	private List<IProduto> produtos1;
+*/
+	private List<Produto> produtos;
+	private List<Produto> produtos1;
 	
 	
-	public <T> List<T> alugarProduto(List<T> produto) {
+	/*public <T> List<T> alugarProduto(List<T> produto) {
 		IProduto p1 = (IProduto) produto;
 		this.produtos.add(p1);
 		return produto;
 	}
 >>>>>>> 1dd5286e86939f4f73a9fb52c6cb1826553f0587
+	 */
 	
 	public Cliente(String nome, String cpf, String telefone, String endereco) {
 		super();
@@ -31,38 +34,7 @@ public class Cliente {
 		this.produtos = new ArrayList<Produto>();
 	}
 	
-	public void addCarro(String nome, double precoPorAluguel, String cor, String chassi, String placa, String modelo, int ano,
-			double motor, int lugares, int portas) {
-		Carro carro = new Carro(nome, precoPorAluguel, cor, chassi, placa, modelo, ano, motor, lugares, portas);
-		this.getProdutos().add(carro);
-	}
-
-	public void addMoto(String nome, double precoPorAluguel, String cor, String chassi, String placa, String modelo, int ano,
-			double motor, int cilindradas) {
-		Moto moto = new Moto(nome, precoPorAluguel, cor, chassi, placa, modelo, ano, motor, cilindradas);
-		this.getProdutos().add(moto);
-	}
-
-	public void addBicicleta(String nome, double precoPorAluguel, String cor, int marchas) {
-		Bicicleta bicicleta = new Bicicleta(nome, precoPorAluguel, cor, marchas);
-		this.getProdutos().add(bicicleta);
-	}
-	
-	public void addManutencao(String data, String hora, double valor) {
-		Manutencao manutencao = new Manutencao(data, hora, valor);
-		this.getProdutos().add(manutencao);
-	}
-	
-	public void addLimpeza(String data, String hora, double valor) {
-		Limpeza limpeza = new Limpeza(data, hora, valor);
-		this.getProdutos().add(limpeza);
-	}
-
-	public void addGuincho(String data, String hora, double valor) {
-		Guincho guincho = new Guincho(data, hora, valor);
-		this.getProdutos().add(guincho);
-	}
-	
+		
 	public String getNome() {
 		return nome;
 	}
